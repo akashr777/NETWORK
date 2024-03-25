41f5002ecf7537b290fa7f39a455e8bba8f886fd
Akash R <133724616+akashr777@users.noreply.github.com>
2024-03-25 14:36:52 +0530

    EXP7

--- triangle.sh
+++ triangle.sh
@@ -1,6 +1,7 @@
-if [ $# -gt 0 ]
+#script to check if the given sides can be the sides of a trianle 
+if [ $# -gt 1 ]
 then
-echo "#syntax error"
+echo "syntax error"
 exit 1
 fi
 if [ $# -eq 0 ]
@@ -12,12 +13,18 @@ read b
 echo "enter third number"
 read c
 fi
-echo "output"
 if [ `expr $a + $b` -le $c -o `expr $a + $c` -le $b -o `expr $b + $c` -le $a ]
 then
-    echo "$a $b $c cannot be the sides of a triangle " 
-    exit
+echo "$a $b $c cannot be the sides of triangle"
 else
-    echo "$a $b $c can be the sides of a triangle"
+echo "$a $b $c can be the sides of triangle"
+   if [ $a -eq $b -a $b -eq $c ]
+   then 
+   echo "Triangle is equilateral"
+   elif [ $a -eq $b -o $b -eq $c -o $a -o $c ]
+   then
+   echo "Triangle is isosceles"
+   else
+   echo "Triangle is scalene"
+   fi
 fi
- 

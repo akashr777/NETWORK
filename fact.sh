465fb42b06247ec74d89a0bc3ec485ba71906307
Akash R <133724616+akashr777@users.noreply.github.com>
2024-05-10 10:35:33 +0530

    EXP 10

---
+++ fact.sh
@@ -0,0 +1,22 @@
+#set -vx
+if [ $# -ne 1 ]
+then 
+echo "Syntax is <$0> <number>"
+exit 1
+fi 
+# function to find factorial
+fact()
+{
+n=$1
+i=1
+f=1
+while [ $i -le $n ]
+do
+f=`expr $f \* $i`
+i=`expr $i + 1`
+done
+#echo "$n != $f"
+return $f
+}
+fact $1
+echo "$1! = $?"
